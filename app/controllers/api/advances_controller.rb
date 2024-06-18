af3dcbbd6f8226829ebfa981de83d0475f89a421
PrimeRin <primedruk312@gmail.com>
2024-06-18 09:42:16 +0600

    add itinerary service

--- app/controllers/api/advances_controller.rb
+++ app/controllers/api/advances_controller.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 module Api
   class AdvancesController < ApplicationController
-    before_action :set_advance, only: [:show]
+    before_action :set_advance, only: [:show, :update]
 
     def index
       @pagy, @advances = pagy(AdvanceQuery.call(params[:advance], current_user, Advance.all).run,
@@ -11,14 +9,26 @@ module Api
     end
 
     def show
-      render json: @advance
+      render json: @advance, status: :ok
     end
 
     def create
       @advance = current_user.advances.new(advance_params)
 
       if @advance.save
+        create_itinerary if itinerary_needed?
+        attach_files if files_attached?
         render json: @advance, status: :created
+      else
+        render json: @advance.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @advance.update(advance_params)
+        update_itinerary if itinerary_needed?
+        update_files if files_params_present?
+        render json: @advance, status: :ok
       else
         render json: @advance.errors, status: :unprocessable_entity
       end
@@ -26,14 +36,38 @@ module Api
 
     private
 
-    def create_iternary; end
+    def attach_files
+      params[:files]&.each { |file| @advance.files.attach(file) }
+    end
 
-    def create_attachment; end
+    def files_attached?
+      params[:files].present? && params[:files].is_a?(Array)
+    end
+
+    def update_itinerary
+      @advance.travel_itineraries.update(itinerary_params)
+    end
+
+    def update_files
+      @advance.files.attach(params[:files][:files])
+    end
+
+    def itinerary_params
+      params.require(:travel_itinerary).permit(:start_date, :end_date, :from, :to)
+    end
+
+    def create_itinerary
+      ItineraryService.new(@advance, itinerary_params).create
+    end
 
     def set_advance
       @advance = Advance.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       render json: { error: 'Advance not found' }, status: :not_found
+    end
+
+    def itinerary_needed?
+      %w[in_country_tour_advance ex_country_tour_advance].include?(@advance.advance_type)
     end
 
     def advance_params
---
+++ app/services/itinerary_service.rb
@@ -0,0 +1,14 @@
+class ItineraryService
+  def initialize(advance, itinerary_params)
+    @advance = advance
+    @itinerary_params = itinerary_params
+  end
+
+  def create
+    @advance.travel_itineraries.create(@itinerary_params)
+  end
+
+  def update(itinerary, new_itinerary_params)
+    itinerary.update(new_itinerary_params)
+  end
+end
