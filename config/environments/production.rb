b48af2ea9a77fe073d703f5c0d9f58b2bf5ee89b
PrimeRin <primedruk312@gmail.com>
2024-07-31 18:43:51 +0600

    update prodution env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,24 +3,31 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  config.cache_classes = true
-  config.eager_load = true
-  config.consider_all_requests_local = false
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.active_storage.service = :local
-  config.log_level = :info
-  config.log_tags = [:request_id]
-  config.action_mailer.perform_caching = false
-  config.i18n.fallbacks = true
-  config.active_support.report_deprecations = false
-  config.log_formatter = ::Logger::Formatter.new
-
-  if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new($stdout)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  config.cache_classes = false
+  config.eager_load = false
+  config.consider_all_requests_local = true
+  config.server_timing = true
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+    config.cache_store = :null_store
   end
-
-  config.active_record.dump_schema_after_migration = false
+  config.action_cable.url = ENV['ACTION_CABLE_URL']
+  config.action_cable.allowed_request_origins = ENV['ACTION_CABLE_ALLOWED_ORIGINS'].split(',')
+  config.active_storage.service = :local
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.active_support.deprecation = :log
+  config.active_support.disallowed_deprecation = :raise
+  config.active_support.disallowed_deprecation_warnings = []
+  config.active_record.migration_error = :page_load
+  config.active_record.verbose_query_logs = true
+  config.middleware.use ActionDispatch::Cookies
+  config.middleware.use ActionDispatch::Session::CookieStore
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   Rails.application.routes.default_url_options[:host] = ENV['RAILS_BACKEND_URL']
 end
