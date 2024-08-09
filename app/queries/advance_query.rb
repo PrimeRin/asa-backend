b2f6be610e431851c76d38ce2676b7059eb3b405
PrimeRin <primedruk312@gmail.com>
2024-08-09 11:17:33 +0600

    return if the query is nil

--- app/queries/advance_query.rb
+++ app/queries/advance_query.rb
@@ -42,7 +42,7 @@ class AdvanceQuery
   end
 
   def search
-    return unless @params[:query]
+    return unless @params[:query].to_s.strip.empty?
 
     @resource = @resource.joins(:user).where('users.email LIKE ?', "%#{@params[:query]}%")
   end
