6a56e1446af20900067ecac39ab570156e86eaba
PrimeRin <primedruk312@gmail.com>
2024-08-12 13:38:44 +0600

    update file controller

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -2,12 +2,24 @@ module Api
   class FilesController < ApplicationController
     include UrlHelper
     before_action :authenticate_user!
-    before_action :set_advance, only: %i[create]
+    before_action :set_advance, only: %i[create update]
 
     def create
       if files_attached?
         params[:files]&.each { |file| @advance.files.attach(file) }
         render status: :created
+      else
+        render status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if files_attached?
+        # Optionally, detach existing files before attaching new ones
+        @advance.files.purge if params[:purge_existing]
+
+        params[:files]&.each { |file| @advance.files.attach(file) }
+        render status: :ok
       else
         render status: :unprocessable_entity
       end
