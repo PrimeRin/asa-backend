843f2ffc1377777c4824520653decc5df5f3121f
PrimeRin <primedruk312@gmail.com>
2024-08-15 16:34:01 +0600

    update uat

--- app/queries/advance_update_query.rb
+++ app/queries/advance_update_query.rb
@@ -43,7 +43,7 @@ class AdvanceUpdateQuery
       @resource.update(status: next_status, message: @params[:message], "#{next_status}_by": @current_user.id)
       if next_status == 'dispatched'
         generate_dispatched_ref
-      elsif next_status == 'confirmed' && @resource.advance_percentage == '0.0'
+      elsif next_status == 'confirmed' && @resource.advance_percentage == 0.0
         @resource.update(status: "dispatched")
         generate_dispatched_ref
       end
