d9dd892e36457e3b3b2075063fbb6dcd659fb268
PrimeRin <primedruk312@gmail.com>
2024-08-15 15:30:50 +0600

    add office order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_15_053954) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_15_092904) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_15_053954) do
     t.json "advance_amount"
     t.json "dispatched_ref"
     t.integer "parent_id"
+    t.string "office_order"
     t.index ["user_id"], name: "index_advances_on_user_id"
   end
 
