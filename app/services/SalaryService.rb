d160a3fa0c06c48d4bf0d771c79342a90fd10764
PrimeRin <primedruk312@gmail.com>
2024-08-19 14:50:39 +0600

    update completion month

--- app/services/SalaryService.rb
+++ app/services/SalaryService.rb
@@ -8,6 +8,7 @@ class SalaryService
 
   def create
     @advance.create_salary_advance(@salary_params).save
+    @advance.update(completion_month: get_completion_month_year)
   end
 
   def update
@@ -18,5 +19,12 @@ class SalaryService
     else
       raise "Salary record not found for advance ID #{@advance.id}"
     end
+  end
+
+  def get_completion_month_year
+    date = @advance.updated_at || Time.now
+    duration = @salary_params[:duration] || 0
+    completion_date = date.advance(months: duration)
+    completion_date.strftime("%B %Y")
   end
 end
