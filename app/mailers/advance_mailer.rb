0a438d22e1ecfcfde77cb3273f0b4aa993120a52
PrimeRin <primedruk312@gmail.com>
2024-09-03 16:33:09 +0600

    email recipient name

--- app/mailers/advance_mailer.rb
+++ app/mailers/advance_mailer.rb
@@ -4,9 +4,8 @@ class AdvanceMailer < ApplicationMailer
     @message = message
     @url = ENV['HOST_DOMAIN']
     user = User.find_by(id: recipient_id)
-    @icbs_user_name = icbs_user(user.username)
     if user
-      @user = user
+      @icbs_user_name = user_full_name(user.username)
       mail(to: @user.email, subject: 'Salary Advance and DSA Notification')
     else
       Rails.logger.warn "User with ID #{recipient_id} not found. Skipping."
