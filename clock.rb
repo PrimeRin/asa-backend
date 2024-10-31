91f159d51d587aaace4a6342982e43e600bdf48d
PrimeRin <primedruk312@gmail.com>
2024-10-31 15:12:46 +0600

    test db back up in uat

--- clock.rb
+++ clock.rb
@@ -9,9 +9,14 @@ handler do |job|
   puts "Running #{job}"
 end
 
-every(1.day, 'dsa_reminder.job', at: '09:00') { DsaReminderJob.perform_later }
+# every(1.day, 'dsa_reminder.job', at: '09:00') { DsaReminderJob.perform_later }
 # every(10.seconds, 'dsa_reminder_job') { DsaReminderJob.perform_later }
-every(1.week, 'db.backup', at: '02:00') do
+every(5.minutes, 'db.backup') do
   puts "Starting weekly database backup at #{Time.now}"
-  system("cd /path/to/your/rails_app && bundle exec rake db:backup")
+  system("cd /home/prime/Desktop/rma/asa-backend && bundle exec rake db:backup")
 end
+
+# every(1.week, 'db.backup', at: '02:00') do
+#   puts "Starting weekly database backup at #{Time.now}"
+#   system("cd /home/prime/Desktop/rma/asa-backend && bundle exec rake db:backup")
+# end
